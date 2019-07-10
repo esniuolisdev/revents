f720f592924da69c9b0199f86ab0d547fb362f7c
esniuolisdev <=>
2019-07-10 09:53:08 +0300

    added my github name

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -4,28 +4,29 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 
+// Test
 const DateInput = ({
-  input,
-  width,
-  placeholder,
-  meta: { touched, error },
-  ...rest
+    input,
+    width,
+    placeholder,
+    meta: { touched, error },
+    ...rest
 }) => {
-  return (
-    <Form.Field error={touched && !!error}>
-      <DatePicker
-        {...rest}
-        placeholderText={placeholder}
-        selected={input.value ? moment(input.values) : null}
-        onChange={input.onChange}
-      />
-      {touched && error && (
-        <Label basic color="red">
-          {error}
-        </Label>
-      )}
-    </Form.Field>
-  );
+    return (
+        <Form.Field error={touched && !!error}>
+            <DatePicker
+                {...rest}
+                placeholderText={placeholder}
+                selected={input.value ? moment(input.values) : null}
+                onChange={input.onChange}
+            />
+            {touched && error && (
+                <Label basic color="red">
+                    {error}
+                </Label>
+            )}
+        </Form.Field>
+    );
 };
 
 export default DateInput;
